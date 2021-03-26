3171f765072de7d9038458b12275de11bb937537
siddharth0600 <siddharthtripathi00@gmail.com>
2021-03-26 17:20:29 +0530

    search date feature complete

--- src/Banner.js
+++ src/Banner.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
   const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
       <div className="banner__search">
-        {showSearch && <h1>Show Date Picker</h1>}
+        {showSearch && <Search />}
 
         <Button
           onClick={() => setShowSearch(!showSearch)}
           className="banner__searchButton"
           variant="outlined"
         >
-          Search Dates
+          {showSearch ? "Hide" : "Search Dates"}
         </Button>
       </div>
       <div className="banner__info">
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Banner from "./Banner";
 import "./Home.css";
+import Card from "./Card";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
