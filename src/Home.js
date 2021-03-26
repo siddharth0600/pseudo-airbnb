a6d6cf8437d25482a4901092111c3b549d015fd4
siddharth0600 <siddharthtripathi00@gmail.com>
2021-03-26 10:09:05 +0530

    Banner done

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Home.css";
 
 function Home() {
-  return <div className="home">{/* <h1>Home component</h1> */}</div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
