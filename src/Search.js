73a613ed10e1ac8e021c078355203235fb9fd1eb
siddharth0600 <siddharthtripathi00@gmail.com>
2021-03-27 20:40:10 +0530

    search page and home page linked

--- src/Search.js
+++ src/Search.js
@@ -5,9 +5,11 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 import { Button } from "@material-ui/core";
 import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 //This is actually date picker component
 function Search() {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -30,7 +32,7 @@ function Search() {
         <PeopleIcon />
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => history.push("./search")}>Search Airbnb</Button>
     </div>
   );
 }
